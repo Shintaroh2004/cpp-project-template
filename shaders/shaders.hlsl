514ced299d966cecb3ea474549f379442db3fcf1
Shintaro2004 <watanabe20040802@gmail.com>
2025-06-14 20:40:26 +0900

    Add D3D12Texture Sample

--- shaders/shaders.hlsl
+++ shaders/shaders.hlsl
@@ -9,12 +9,26 @@
 //
 //*********************************************************
 
-#include "stdafx.h"
-#include "D3D12HelloTexture.h"
-
-_Use_decl_annotations_
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+struct PSInput
 {
-    D3D12HelloTexture sample(1280, 720, L"D3D12 Hello Texture");
-    return Win32Application::Run(&sample, hInstance, nCmdShow);
+    float4 position : SV_POSITION;
+    float2 uv : TEXCOORD;
+};
+
+Texture2D g_texture : register(t0);
+SamplerState g_sampler : register(s0);
+
+PSInput VSMain(float4 position : POSITION, float4 uv : TEXCOORD)
+{
+    PSInput result;
+
+    result.position = position;
+    result.uv = uv;
+
+    return result;
+}
+
+float4 PSMain(PSInput input) : SV_TARGET
+{
+    return g_texture.Sample(g_sampler, input.uv);
 }
--- src/stdafx.cpp
+++ src/stdafx.cpp
@@ -9,12 +9,4 @@
 //
 //*********************************************************
 
-#include "stdafx.h"
-#include "D3D12HelloTexture.h"
-
-_Use_decl_annotations_
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
-{
-    D3D12HelloTexture sample(1280, 720, L"D3D12 Hello Texture");
-    return Win32Application::Run(&sample, hInstance, nCmdShow);
-}
+#include <stdafx.h>
